498eece00947383c6cef9ee4592fe23954196a29
solara <sameperson3108@gmail.com>
2026-03-04 09:47:56 +0700

    initital commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
-public class Main {
+public class Main { // нахожусь в класс main
+    public static void main(String[] args) { // точка входа в программу
+        /*
+        sout выводит на консоль информацию, указанную в скобках
+         */
+        System.out.println("My name is solara");
+    }
 }
